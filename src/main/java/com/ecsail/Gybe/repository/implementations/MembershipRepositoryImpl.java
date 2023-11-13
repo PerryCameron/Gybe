3da73ce6309155058d8e82f27bb048dad5ec9c59
PerryCameron <pcameron28@yahoo.com>
2023-11-13 14:43:44 -0500

    updgraded getMembershipByMsId method to pull all info

--- src/main/java/com/ecsail/Gybe/repository/implementations/MembershipRepositoryImpl.java
+++ src/main/java/com/ecsail/Gybe/repository/implementations/MembershipRepositoryImpl.java
@@ -186,12 +186,13 @@ public class MembershipRepositoryImpl implements MembershipRepository {
     @Override
     public MembershipListDTO getMembershipByMsId(int msId) {
         String query = """
-                SELECT m.ms_id,m.p_id,id.membership_id,id.fiscal_year,m.join_date,
-                id.mem_type,p.l_name,p.f_name,m.address,m.city,m.state,m.zip FROM 
-                membership m LEFT JOIN person p ON m.p_id=p.p_id LEFT JOIN membership_id 
-                id ON m.ms_id=id.ms_id WHERE id.fiscal_year=YEAR(NOW()) AND m.MS_ID=?
-                """;
-        return template.queryForObject(query, new MembershipListRowMapper1(), msId);
+                Select m.MS_ID,m.P_ID,id.MEMBERSHIP_ID,id.FISCAL_YEAR,id.FISCAL_YEAR,m.JOIN_DATE, 
+                id.MEM_TYPE,s.SLIP_NUM,p.L_NAME,p.F_NAME,s.SUBLEASED_TO,m.address,m.city,m.state,m.zip 
+                from slip s right join membership m on m.MS_ID=s.MS_ID left join membership_id id on 
+                m.MS_ID=id.MS_ID left join person p on p.MS_ID=m.MS_ID where p.MEMBER_TYPE=1 
+                AND id.fiscal_year=YEAR(NOW()) AND m.MS_ID=958
+                                """;
+        return template.queryForObject(query, new MembershipListRowMapper(), msId);
     }
 
     @Override
--- src/main/java/com/ecsail/Gybe/service/MembershipService.java
+++ src/main/java/com/ecsail/Gybe/service/MembershipService.java
@@ -1,21 +1,29 @@
 package com.ecsail.Gybe.service;
 
 import com.ecsail.Gybe.dto.MembershipListDTO;
+import com.ecsail.Gybe.dto.PersonDTO;
 import com.ecsail.Gybe.repository.interfaces.MembershipRepository;
+import com.ecsail.Gybe.repository.interfaces.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class MembershipService {
     private final MembershipRepository membershipRepository;
+    private final PersonRepository personRepository;
 
     @Autowired
-    public MembershipService(MembershipRepository membershipRepository) {
+    public MembershipService(MembershipRepository membershipRepository, PersonRepository personRepository) {
         this.membershipRepository = membershipRepository;
+        this.personRepository = personRepository;
     }
 
     public MembershipListDTO getMembership(int msId) {
-        return membershipRepository.getMembershipByMsId(msId);
+        MembershipListDTO membership = membershipRepository.getMembershipByMsId(msId);
+        membership.setPersonDTOS((ArrayList<PersonDTO>) personRepository.getActivePeopleByMsId(msId));
+        return membership;
     }
 
 }
