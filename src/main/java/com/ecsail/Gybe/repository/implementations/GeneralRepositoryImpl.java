76d03b966c20a42a2594bdf89253c8b3ca2b8ba5
PerryCameron <pcameron28@yahoo.com>
2024-02-28 10:43:27 -0500

    Added repo for stats

--- src/main/java/com/ecsail/Gybe/repository/implementations/GeneralRepositoryImpl.java
+++ src/main/java/com/ecsail/Gybe/repository/implementations/GeneralRepositoryImpl.java
@@ -1,12 +1,15 @@
 package com.ecsail.Gybe.repository.implementations;
 
+import com.ecsail.Gybe.dto.StatsDTO;
 import com.ecsail.Gybe.repository.interfaces.GeneralRepository;
+import com.ecsail.Gybe.repository.rowmappers.StatsRowMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 @Repository
 public class GeneralRepositoryImpl implements GeneralRepository {
@@ -23,6 +26,11 @@ public class GeneralRepositoryImpl implements GeneralRepository {
     public boolean recordExists(String tableName, String columnName, int value) {
         String EXISTS_QUERY = "SELECT EXISTS(SELECT * FROM " + tableName + " WHERE " + columnName + "=?)";
         return Boolean.TRUE.equals(template.queryForObject(EXISTS_QUERY, Boolean.class, new Object[]{value}));
+    }
+
+    public List<StatsDTO> getAllStats() {
+        String sql = "SELECT * FROM stats";
+        return template.query(sql, new StatsRowMapper());
     }
 
 }
