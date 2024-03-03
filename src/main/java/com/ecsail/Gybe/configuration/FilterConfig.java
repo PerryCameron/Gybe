802247b1671f212e2a305ae885aa64b5002f0cf9
PerryCameron <pcameron28@yahoo.com>
2024-03-03 18:27:55 -0500

    Added security config which would allow export of html to ecsail.org

---
+++ src/main/java/com/ecsail/Gybe/configuration/FilterConfig.java
@@ -0,0 +1,17 @@
+package com.ecsail.Gybe.configuration;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public FilterRegistrationBean<SecurityHeadersFilter> securityHeadersFilter() {
+        FilterRegistrationBean<SecurityHeadersFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new SecurityHeadersFilter());
+        registrationBean.addUrlPatterns("/*");
+        return registrationBean;
+    }
+}
---
+++ src/main/java/com/ecsail/Gybe/configuration/SecurityHeadersFilter.java
@@ -0,0 +1,24 @@
+package com.ecsail.Gybe.configuration;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SecurityHeadersFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        httpResponse.setHeader("X-Frame-Options", "SAMEORIGIN");
+        httpResponse.setHeader("Content-Security-Policy", "frame-ancestors 'self' https://www.ecsail.org");
+        chain.doFilter(request, response);
+    }
+}
+//    @Override
+//    public void init(FilterConfig filterConfig) {
+//    }
+
+//    @Override
+//    public void destroy() {
+//    }
+
