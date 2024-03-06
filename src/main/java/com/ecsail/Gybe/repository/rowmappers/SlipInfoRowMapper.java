1093cb1d8176094c6e20fe036b12d3a7a9f22751
PerryCameron <pcameron28@yahoo.com>
2024-03-06 10:40:27 -0500

    created slip info row mapper

---
+++ src/main/java/com/ecsail/Gybe/repository/rowmappers/SlipInfoRowMapper.java
@@ -0,0 +1,27 @@
+package com.ecsail.Gybe.repository.rowmappers;
+
+
+import com.ecsail.Gybe.dto.RoleDTO;
+import com.ecsail.Gybe.dto.SlipInfoDTO;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class SlipInfoRowMapper implements RowMapper<SlipInfoDTO> {
+    @Override
+    public SlipInfoDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new SlipInfoDTO(
+                rs.getInt("owner_id"),
+                rs.getString("owner_first_name"),
+                rs.getString("owner_last_name"),
+                rs.getInt("owner_msid"),
+                rs.getString("slip_number"),
+                rs.getString("alt_text"),
+                rs.getInt("subleaser_id"),
+                rs.getInt("subleaser_msid"),
+                rs.getString("subleaser_first_name"),
+                rs.getString("subleaser_last_name"));
+    }
+}
+
