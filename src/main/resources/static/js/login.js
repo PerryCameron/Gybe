ed7234f5860be61c76216d5fd73a195d83fb52d1
PerryCameron <pcameron28@yahoo.com>
2024-04-10 09:52:55 -0400

    updated email page to look nicer

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,56 +1,59 @@
-function showEmailField() {
-  // Get the form element
-  let form = document.getElementById("loginForm");
-
-  // Store the CSRF token value
-  let csrfToken = document.querySelector('input[name="_csrf"]').value;
-
-  // Clear the form
-  form.innerHTML = "";
-  form.action = '/upsert_user';
-
-  let outerDiv = document.createElement("div");
-  outerDiv.classList.add("form-group");
-  let buttonDiv = document.createElement("div");
-  buttonDiv.classList.add("form-group", "submit-button-div");
-
-  // Create a new input field for the email
-  outerDiv.appendChild(addEmailField());
-  outerDiv.appendChild(buttonDiv);
-
-  // Create a new submit button
-  let submitButton = document.createElement("button");
-  submitButton.type = "submit";
-  submitButton.textContent = "Submit";
-  buttonDiv.appendChild(submitButton);
-
-  // Append the new fields to the form
-  form.appendChild(outerDiv);
-
-  // Add the CSRF token field back to the form
-  let csrfInput = document.createElement("input");
-  csrfInput.type = "hidden";
-  csrfInput.name = "_csrf";
-  csrfInput.value = csrfToken;
-  form.appendChild(csrfInput);
+function buildEmailView() {
+    const form = document.getElementById("loginForm");
+    // Store the CSRF token value
+    const csrfToken = document.querySelector('input[name="_csrf"]').value;
+    form.innerHTML = "";
+    const div = document.createElement("div");
+    div.classList.add("form-group");
+    div.appendChild(addSubScript());
+    div.appendChild(addEmailFieldAndButton());
+    form.appendChild(div);
+    // Add the CSRF token field back to the form
+    const csrfInput = document.createElement("input");
+    csrfInput.type = "hidden";
+    csrfInput.name = "_csrf";
+    csrfInput.value = csrfToken;
+    form.appendChild(csrfInput);
 }
 
+function addEmailFieldAndButton() {
+    const div = document.createElement("div");
+    div.classList.add("email-div");
+    div.appendChild(addEmailField());
+    div.appendChild(addButton());
+    return div;
+}
+
+function addButton() {
+    const div = document.createElement("div");
+    div.classList.add("form-group", "submit-button-div");
+    const submitButton = document.createElement("button");
+    submitButton.type = "submit";
+    submitButton.textContent = "Submit";
+    div.appendChild(submitButton);
+    return div;
+}
 
 function addEmailField() {
-  let emailDiv = document.createElement("div");
-  emailDiv.classList.add("subscript-div");
-  let emailInput = document.createElement("input");
-  emailInput.type = "email";
-  emailInput.name = "email";
-  emailInput.id = "email";
-  emailInput.placeholder = "Email";
-  emailInput.required = true;
-  emailDiv.appendChild(emailInput);
-  const subscript = document.createElement("label");
-  subscript.htmlFor = "main-input";
-  subscript.className = "subscript";
-  subscript.textContent =
-    "Enter email used for account or to register for first time use any email listed in your membership";
-  emailDiv.appendChild(subscript);
-  return emailDiv;
+    const div = document.createElement("div");
+    div.classList.add("email-div");
+    const input = document.createElement("input");
+    input.type = "email";
+    input.name = "email";
+    input.placeholder = "Email";
+    input.required = true;
+    div.appendChild(input);
+    return div;
+}
+
+function addSubScript() {
+    let div = document.createElement("div");
+    div.classList.add("subscript-div");
+    const label = document.createElement("label");
+    label.htmlFor = "main-input";
+    label.className = "subscript";
+    label.textContent =
+        "Enter email used for account or to register for first time use any email listed in your membership";
+    div.appendChild(label);
+    return div;
 }
---
+++ src/main/resources/static/js/login.old.js
@@ -0,0 +1,56 @@
+function showEmailField() {
+  // Get the form element
+  let form = document.getElementById("loginForm");
+
+  // Store the CSRF token value
+  let csrfToken = document.querySelector('input[name="_csrf"]').value;
+
+  // Clear the form
+  form.innerHTML = "";
+  form.action = '/upsert_user';
+
+  let outerDiv = document.createElement("div");
+  outerDiv.classList.add("form-group");
+  let buttonDiv = document.createElement("div");
+  buttonDiv.classList.add("form-group", "submit-button-div");
+
+  // Create a new input field for the email
+  outerDiv.appendChild(addEmailField());
+  outerDiv.appendChild(buttonDiv);
+
+  // Create a new submit button
+  let submitButton = document.createElement("button");
+  submitButton.type = "submit";
+  submitButton.textContent = "Submit";
+  buttonDiv.appendChild(submitButton);
+
+  // Append the new fields to the form
+  form.appendChild(outerDiv);
+
+  // Add the CSRF token field back to the form
+  let csrfInput = document.createElement("input");
+  csrfInput.type = "hidden";
+  csrfInput.name = "_csrf";
+  csrfInput.value = csrfToken;
+  form.appendChild(csrfInput);
+}
+
+
+function addEmailField() {
+  let emailDiv = document.createElement("div");
+  emailDiv.classList.add("subscript-div");
+  let emailInput = document.createElement("input");
+  emailInput.type = "email";
+  emailInput.name = "email";
+  emailInput.id = "email";
+  emailInput.placeholder = "Email";
+  emailInput.required = true;
+  emailDiv.appendChild(emailInput);
+  const subscript = document.createElement("label");
+  subscript.htmlFor = "main-input";
+  subscript.className = "subscript";
+  subscript.textContent =
+    "Enter email used for account or to register for first time use any email listed in your membership";
+  emailDiv.appendChild(subscript);
+  return emailDiv;
+}
